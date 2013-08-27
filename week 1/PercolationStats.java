c88088ac4214ed143a12a1875339f7e10c09cd45
Victor <no_mail_at_all@mail.ru>
2013-08-27 23:25:24 +0400

    PercolationStats finished

--- week 1/PercolationStats.java
+++ week 1/PercolationStats.java
@@ -2,25 +2,31 @@ import java.util.*;
 
 public class PercolationStats {
     
-    private boolean[][] board;
+   private double fractions[];
+   private int repeat_number;   
    
-   //public PercolationStats(int N, int T)    // perform T independent computational experiments on an N-by-N grid
-   //public double mean()                     // sample mean of percolation threshold
-   //public double stddev()                   // sample standard deviation of percolation threshold
-   //public double confidenceLo()             // returns lower bound of the 95% confidence interval
-   //public double confidenceHi()             // returns upper bound of the 95% confidence interval
-    public static void main(String[] args)   // test client, described below
-    {
-        System.out.println("Start...");
-        int p = 0;
-        int q = 0;
-        int counter = 0;        
-        Percolation test = new Percolation(10);
-        while (!test.percolates())
-        {
+   public PercolationStats(int N, int T)    // perform T independent computational experiments on an N-by-N grid
+   {
+       if( N <= 0 || T <= 0 )
+           throw new IndexOutOfBoundsException("ERROR: Incorrect input data");
+       
+       fractions = new double[T];
+       Arrays.fill(fractions, 0);
+       
+       repeat_number = T;
+                  
+       for (int i = 0; i < T; i++)
+       {
+           Percolation test = new Percolation(N);
+           int p = 0;
+           int q = 0;
+           int counter = 0;        
+           
+           while (!test.percolates())
+           {
             
-                p = 1 + (int) (Math.random() * 10);
-                q = 1 + (int) (Math.random() * 10);
+                p = 1 + (int) (Math.random() * N);
+                q = 1 + (int) (Math.random() * N);
                 
                 if(test.isFull(p,q))
                 {
@@ -29,10 +35,52 @@ public class PercolationStats {
                     //test.dump();                    
                     counter++;
                 }
-        }
-        test.dump();                    
-        System.out.println(counter);
-        
-    }
+           }
+        //test.dump();                    
+        fractions[i]=(double)counter / N / N;
+       }     
+       
+   }
+
+   public double mean()                     // sample mean of percolation threshold
+   {
+       double sum = 0;
+       
+       for (int i = 0; i < fractions.length; i++) 
+            sum += fractions[i];
+       
+       return (sum/repeat_number);
+   }
+   
+   public double stddev()                   // sample standard deviation of percolation threshold
+   {
+       double mean = this.mean();
+       double stddev = 0;
+       
+       for (int i = 0; i < fractions.length; i++) 
+            stddev += Math.pow((fractions[i] - mean), 2);
+       
+       return Math.sqrt( stddev / (repeat_number - 1));   
+       
+   }
+   
+   public double confidenceLo()             // returns lower bound of the 95% confidence interval
+   {
+       return (mean() - (double)1.96 * stddev() / Math.sqrt(repeat_number) );
+   }
+       
+   public double confidenceHi()             // returns upper bound of the 95% confidence interval
+   {
+       return (mean() + (double)1.96 * stddev() / Math.sqrt(repeat_number) );
+   }
+       
+   public static void main(String[] args)   // test client, described below
+   {
+       PercolationStats stat = new PercolationStats (Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+       System.out.println("mean\t\t\t\t = " + stat.mean());
+       System.out.println("stddev\t\t\t\t = " + stat.stddev());
+       System.out.println("95% confidence interval\t\t = " + stat.confidenceLo() + " " + stat.confidenceHi());
+       
+   }
        
 }
